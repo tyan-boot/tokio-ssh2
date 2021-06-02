5658671374182cd9806f51854eea9b82f9de4bfe
Tyan Boot <tyanboot@outlook.com>
2021-06-02 11:47:43 +0800

    expose AsyncFile

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ pub use agent::AsyncAgent;
 pub use channel::{AsyncChannel, AsyncStream};
 pub use listener::AsyncListener;
 pub use session::AsyncSession;
-pub use sftp::AsyncSftp;
+pub use sftp::{AsyncSftp, AsyncFile};
 
 mod agent;
 mod channel;
